59080ff0e50a78ef1b63218c7e3220f0d77d2eed
Danello <ATL30165@rsui.com>
2018-07-31 14:00:23 -0400

    Getting Resources

--- CityInfo.API/Models/PointOfInterestDto.cs
+++ CityInfo.API/Models/PointOfInterestDto.cs
@@ -5,16 +5,11 @@ using System.Threading.Tasks;
 
 namespace CityInfo.API.Models
 {
-    public class CityDto
+    public class PointOfInterestDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int NumberOfPointsOfInterest
-        {
-            get { return PointsOfInterest.Count; }
-        }
 
-        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
     }
 }
